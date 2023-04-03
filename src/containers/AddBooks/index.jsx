a7531c769ab15dbb9c95ea6b9176def404e1c6e4
TimotheGonin <gonin.timothe@gmail.com>
2023-04-03 12:52:19 +0200

    🚧 📦 add Bootstrap + app primary skull and styles

---
+++ src/containers/AddBooks/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const AddBooks = () => {
+	return (
+		<main role="main">
+			<div className="jumbotron jumbotron-fluid">
+				<div className="container text-center">
+					<h1 className="display-4">BOOKS</h1>
+					<p>Ajouter un livre à votre bibliothèque</p>
+					<form className="form-inline justify-content-center">
+						<div className="form-group">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Titre"
+								required
+							/>
+						</div>
+						<div className="form-group">
+							<input
+								type="text"
+								className="form-control ml-3"
+								placeholder="Auteur"
+								required
+							/>
+						</div>
+						<div className="form-group">
+							<button className="btn btn-outline-secondary ml-3">
+								Ajouter un livre
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div className="Container" style={{ minHeight: "200px" }}>
+				<div className="row">
+					<div className="col-md-12">
+						<ul className="list-group">
+							<li className="list-group-item list-group-item-light d-flex justify-content-between">
+								livres enregistrés ici
+							</li>
+						</ul>
+						<div className="d-flex justify-content-center">
+							<button className="btn btn-danger mt-4 mb-5">
+								Effacer tous les livres
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default AddBooks;
